bef0dee1ed7a8bf27901e6eacb063ec40c78c6a8
project <project@project-ProLiant-DL380-G3.(none)>
2014-02-24 11:03:46 +0800

    big modification

---
+++ Data/BR-Sa3/ReadMe_First_AmeriFlux_Fair_Use_Policy.txt
@@ -0,0 +1,21 @@
+                  AmeriFlux Fair-Use Data Agreement
+
+The AmeriFlux data provided on this FTP server are freely available and
+were furnished by individual AmeriFlux scientists who encourage their use.
+Please kindly inform the appropriate AmeriFlux scientist(s) of how you
+intend to use the data and of any publication plans.  It is also important
+to contact the AmeriFlux investigator to assure you are downloading
+the latest revision of the data and to prevent potential misuse or
+misinterpretation of the data.  Please acknowledge the data source
+as a citation or in the acknowledgments if no citation is available.
+If the AmeriFlux Principal Investigators (PIs) feel that they should
+be acknowledged or offered participation as authors, they will let you
+know and we assume that an agreement on such matters will be reached
+before publishing and/or use of the data for publication.  If your
+work directly competes with the PI's analysis they may ask that they
+have the opportunity to submit a manuscript before you submit one that
+uses unpublished data.  In addition, when publishing, please acknowledge
+the agency that supported the research.  Lastly, we kindly request that
+those publishing papers using AmeriFlux data provide preprints to the
+PIs providing the data and to the data archive at the Carbon Dioxide
+Information Analysis Center (CDIAC).
---
+++ Pre-processing/WrfConvert/WrfOut2LSMS.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# set the year of process 
+export YEAR=2008
+
+#delete some netcdf file after run the script
+rm ${YEAR}-*.del.nc
+rm ${YEAR}-*.ok.nc
+rm ${YEAR}-*.all.nc
+rm ${YEAR}-*.PRECTmms.nc
+
+# process rainc
+# month
+for i in 01 02 03 04 05 06 07 08 09 10 11 12
+do
+
+    # get data from Pan Xiaoduo Directory, and process some variables
+    cdo selvar,Q2,T2,U10,V10,PSFC,RAINC,SWDOWN ~panxiaoduo/QZ/WRFV3/test/em_real/wrfout_d02_${YEAR}-${i}-01_00:00:00 ${YEAR}-${i}-wrfout.nc
+	cdo expr,'WIND=sqrt(sqr(U10)+sqr(V10))' ${YEAR}-${i}-wrfout.nc ${YEAR}-${i}.wind.nc
+	cdo selvar,RAINC ${YEAR}-${i}-wrfout.nc ${YEAR}-${i}.rainc.nc
+
+    # forward to the first hour
+    cdo shifttime,-1hours ${YEAR}-${i}.rainc.nc ${YEAR}-${i}.rainc.del.nc
+    
+	# delete the other month
+    cdo selmon,${i} ${YEAR}-${i}.rainc.del.nc ${YEAR}-${i}.rainc.to.nc
+
+	# process of WIND
+    cdo shifttime,-1hours ${YEAR}-${i}.wind.nc ${YEAR}-${i}.wind.del.nc
+    cdo selmon,${i} ${YEAR}-${i}.wind.del.nc ${YEAR}-${i}.wind.ok.nc
+
+	# modify variable names
+	cdo chname,Q2,QBOT,T2,TBOT,PSFC,PSRF,SWDOWN,FSDS ${YEAR}-${i}-wrfout.nc ${YEAR}-${i}.chname.nc
+    cdo shifttime,-1hours ${YEAR}-${i}.chname.nc ${YEAR}-${i}.chname.del.nc
+    cdo selmon,${i} ${YEAR}-${i}.chname.del.nc ${YEAR}-${i}.chname.ok.nc
+
+    # hour
+    for j in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+    do
+        #convert unit mm to mm/s
+        cdo seltime,${j}:00:00 ${YEAR}-${i}.rainc.to.nc ${YEAR}-${i}.rainc.to.${j}.nc
+        cdo expr,"RAINC=RAINC/(60*60*(${j}+1))" ${YEAR}-${i}.rainc.to.${j}.nc ${YEAR}-${i}.rainc.to.${j}.expr.nc
+    done
+
+    # merge all timestep
+    cdo mergetime ${YEAR}-${i}.rainc.to.*.expr.nc ${YEAR}-${i}.rainc.ok.nc
+
+    # delete redundancy netcdf file
+    rm ${YEAR}-${i}.rainc.del.nc
+    rm ${YEAR}-${i}.rainc.to*.nc
+    rm ${YEAR}-${i}.wind.del.nc
+    rm ${YEAR}-${i}.chname.del.nc
+
+	# change RAINC to PRECTmms
+    cdo chname,RAINC,PRECTmms ${YEAR}-${i}.rainc.ok.nc ${YEAR}-${i}.rainc.PRECTmms.nc
+    rm ${YEAR}-${i}.rainc.ok.nc
+
+    # merge PRECTmms, WIND, CHNAME to ALL
+    cdo merge ${YEAR}-${i}.chname.ok.nc ${YEAR}-${i}.rainc.PRECTmms.nc ${YEAR}-${i}.wind.ok.nc ${YEAR}-${i}.all.nc
+	
+    # modify the attributions
+	ncdump ${YEAR}-${i}-all.nc | sed -e "s/x/lon/g" | sed -e "s/y/lat/g" | sed -e "s/XLAT/LATIXY/g" | sed -e "s/XLONG/LONGXY/g" |ncgen -o ${YEAR}-${i}.nc
+	
+    mv ${YEAR}-${i}.nc QZ_CLM_Forcing/${YEAR}-${i}.nc
+done
